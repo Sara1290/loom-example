749ed2d78986c0e61269ec89c9131b8adaea4ddf
sara reed <sara.reed@previdence.com>
2021-10-18 16:12:28 -0600

    video up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import * as loom from '@loomhq/loom-embed'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    HELLO THIS IS MY REACT PROJECT WITH LOOOOOM
+      <div style={{position: "relative", paddingBottom: "56.25%", height: "0"}}>
+        <iframe src="https://www.loom.com/embed/1f13bbaab67d47a89cf2831968f0e42b" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style={{position: "absolute", top: "0", left: "0", width: "100%", height: "100%"}}>
+        </iframe>
+      </div>
     </div>
   );
 }
